2e6fcaa7cb93f5cd55ab1276389f281d0df9f716
CountTails <74679703+CountTails@users.noreply.github.com>
2024-12-20 19:27:16 -0800

    Feat(appinit): application entry point accepts config file options and initializes logging facilities

--- src/match_scheduler_bot/__init__.py
+++ src/match_scheduler_bot/__init__.py
@@ -4,3 +4,20 @@
     :module_author: CountTails
 """
 
+import logging
+import logging.config
+import logging.handlers
+import json
+import atexit
+
+LOGGER = logging.getLogger(__name__)
+
+
+def setup_logging(config: str) -> None:
+    with open(config) as f_in:
+        logconfig = json.load(f_in)
+    logging.config.dictConfig(logconfig)
+    qhandler = logging.getHandlerByName("queue_handler")
+    if qhandler:
+        qhandler.listener.start()
+        atexit.register(qhandler.listener.stop)
--- src/match_scheduler_bot/cli.py
+++ src/match_scheduler_bot/cli.py
@@ -5,8 +5,36 @@
 """
 
 import click
+import os.path
+import time
+
+from . import LOGGER, setup_logging
+
+
+def slow_task():
+    for x in range(1, 11):
+        time.sleep(5)
+        LOGGER.debug("Step %d of some slow task completed", x)
+
 
 @click.command()
-def matchschedulerbot():
+@click.option(
+    '--bot-config',
+    type=click.Path(exists=True),
+    default=f'{os.path.expanduser("~/.config/msa/bot.json")}'
+)
+@click.option(
+    '--log-config',
+    type=click.Path(exists=True),
+    default=f'{os.path.expanduser("~/.config/msa/logging.json")}'
+)
+def matchschedulerbot(bot_config, log_config):
     """Entry point to matchschedulerbot"""
-    click.echo('Hello World!')
+    click.echo(f"Grabbing bot config from: {bot_config}")
+    click.echo(f"Grabbing logging config from: {log_config}")
+    setup_logging(log_config)
+    LOGGER.info("Application successfully initialized. Starting slow task")
+    time.sleep(3)
+    slow_task()
+    time.sleep(3)
+    LOGGER.info("Slow task completed. Exiting...")
