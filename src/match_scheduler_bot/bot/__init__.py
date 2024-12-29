5521bbaa350fda2bb40b92043c7f920f170aa4ae
CountTails <74679703+CountTails@users.noreply.github.com>
2024-12-28 18:22:22 -0800

    Feat(appcli): define the slash commands available with this bot

--- src/match_scheduler_bot/bot/__init__.py
+++ src/match_scheduler_bot/bot/__init__.py
@@ -27,8 +27,22 @@ def setup_bot():
     async def on_ready():
         await bot.tree.sync()
 
-    @bot.tree.command()
-    async def hello(interaction: discord.Interaction, arg: str = 'world'):
-        await interaction.response.send_message(f'Hello {arg}')
+    @bot.tree.command(name='addmatch')
+    async def add_match(interaction: discord.Interaction, home: str, away: str):
+        await interaction.response.send_message(
+            f'Scheduling a match: {away} vs {home}'
+        )
+
+    @bot.tree.command(name='delmatch')
+    async def del_match(interaction: discord.Interaction, home: str, away: str):
+        await interaction.response.send_message(
+            f'Canceling the match between {away} vs {home}'
+        )
+
+    @bot.tree.command(name='showmatches')
+    async def show_matches(interaction: discord.Intents):
+        await interaction.response.send_message(
+            'Showing the upcoming matches'
+        )
 
     return bot
