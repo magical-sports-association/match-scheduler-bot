36ade201dc56b60d35c8d3327fbcc77b0ee88e59
CountTails <74679703+CountTails@users.noreply.github.com>
2024-12-31 20:25:49 -0800

    Fix(dead-code): removed config models that are unused

--- src/match_scheduler_bot/model/config.py
+++ src/match_scheduler_bot/model/config.py
@@ -15,44 +15,11 @@ class BotAuthInfo(pydantic.BaseModel):
     intents: Dict[str, bool]
 
 
-class RolesList(pydantic.BaseModel):
-    staff: str
-    captain: str
-    subscriber: str
-
-
-class UsageGrants(pydantic.BaseModel):
-    addmatch: List[str]
-    cancelmatch: List[str]
-    showmatches: List[str]
-
-
-class AddMatchBehavior(pydantic.BaseModel):
-    earliest: Annotated[int, pydantic.Field(gt=0)]
-    latest: Annotated[int, pydantic.Field(gt=0)]
-
-
-class CancleMatchBehavior(pydantic.BaseModel):
-    latest: Annotated[int, pydantic.Field(gt=0)]
-
-
-class ShowMatchesBehavior(pydantic.BaseModel):
-    limit: Annotated[int, pydantic.Field(gt=0)]
-
-
-class CommandBehaviors(pydantic.BaseModel):
-    addmatch: AddMatchBehavior
-    cancelmatch: CancleMatchBehavior
-    showmatches: ShowMatchesBehavior
-
-
 class PersistentStorage(pydantic.BaseModel):
     database: str | pathlib.Path
+    logs: str | int
 
 
 class BotConfig(pydantic.BaseModel):
     auth: BotAuthInfo
-    roles: RolesList
-    permissions: UsageGrants
-    commands: CommandBehaviors
     storage: PersistentStorage
