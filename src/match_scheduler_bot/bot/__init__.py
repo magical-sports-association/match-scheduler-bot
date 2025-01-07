c6b6ed68f60e949c959b6fe1062ed0d989c5eb20
CountTails <74679703+CountTails@users.noreply.github.com>
2025-01-06 16:35:50 -0800

    Feat(bot-config): configuration simplified to remove response details from config; prepared to rework bot responses and match list management

--- src/match_scheduler_bot/bot/__init__.py
+++ src/match_scheduler_bot/bot/__init__.py
@@ -12,18 +12,10 @@ from discord.ext import commands
 
 from ..model import get_config
 from ..model.matchlist import MatchListRepository, ScheduledMatch
-from ..model.config import BotCommand
+from ..model.config import CommandSpec
 from ..exceptions import MatchSchedulingException
 from .autocomplete import autocomplete_timezone
 from .validators import date_parts
-from .responses import (
-    make_scheduling_success_message,
-    make_scheduling_failure_message,
-    make_cancellation_success_message,
-    make_cancellation_failure_message,
-    make_match_calendar_message
-)
-from .announcements import make_match_callendar_modified_announcement
 
 
 LOGGER = logging.getLogger(__name__)
@@ -38,16 +30,19 @@ def setup_bot():
     )
     config = get_config()
     matchlist = MatchListRepository(config.storage.database)
-    addmatch_options: BotCommand = config.get_command_info('schedule-match')
-    delmatch_options: BotCommand = config.get_command_info('cancel-match')
-    showmatches_options: BotCommand = config.get_command_info(
-        'scheduled-matches')
+    addmatch_options: CommandSpec = config.commands.get('create_match')
+    delmatch_options: CommandSpec = config.commands.get('delete_match')
+    showmatches_options: CommandSpec = config.commands.get('list_matches')
+    helpcommand_options: CommandSpec = config.commands.get('command_help')
 
     @bot.event
     async def on_ready():
         await bot.tree.sync()
 
-    @bot.tree.command(name=addmatch_options.command_name)
+    @bot.tree.command(
+        name=addmatch_options.invoke_with,
+        description=addmatch_options.description
+    )
     @discord.app_commands.describe(
         **{
             param: info.helptext
@@ -65,8 +60,8 @@ def setup_bot():
     )
     async def add_match(
         interaction: discord.Interaction,
-        home_team: discord.Role,
-        away_team: discord.Role,
+        team_1: discord.Role,
+        team_2: discord.Role,
         year: discord.app_commands.Range[int, datetime.MINYEAR, datetime.MAXYEAR],
         month: discord.app_commands.Range[int, 1, 12],
         day: discord.app_commands.Range[int, 1, 31],
@@ -74,66 +69,12 @@ def setup_bot():
         minute: discord.app_commands.Range[int, 0, 59],
         timezone: str
     ):
-        try:
-            match_start: datetime.datetime = date_parts(
-                year=year,
-                month=month,
-                day=day,
-                hour=hour,
-                minute=minute,
-                tzkey=timezone
-            )
-            LOGGER.debug(
-                'Start time parameters successfully validated and converted into aware datetime'
-            )
-            match_to_schedule = ScheduledMatch(
-                scheduled_timestamp=int(match_start.timestamp()),
-                home_team=home_team.id,
-                away_team=away_team.id,
-                scheduled_at=int(datetime.datetime.now(
-                    tz=datetime.timezone.utc
-                ).timestamp()),
-                scheduled_by=interaction.user.id
-            )
-            with matchlist as db:
-                db.schedule_match(match_to_schedule)
+        pass
 
-        except MatchSchedulingException as err:
-            LOGGER.error(
-                'Match could not be scheduled: %s',
-                str(err)
-            )
-            await interaction.response.send_message(
-                embed=make_scheduling_failure_message(
-                    interaction,
-                    err,
-                    addmatch_options.direct_response_not_ok
-                ),
-                ephemeral=True
-            )
-        else:
-            LOGGER.info('Match scheduled successfully')
-            await interaction.response.send_message(
-                embed=make_scheduling_success_message(
-                    interaction,
-                    match_to_schedule,
-                    addmatch_options.direct_response_ok
-                ),
-                ephemeral=True
-            )
-            for channel in addmatch_options.task_completed_message.output_channels:
-                if channel.log_to_text_channel:
-                    await make_match_callendar_modified_announcement(
-                        addmatch_options.task_completed_message,
-                        interaction.guild.get_channel(
-                            channel.text_channel_id
-                        ),
-                        home_team,
-                        away_team,
-                        int(match_start.timestamp())
-                    )
-
-    @bot.tree.command(name=delmatch_options.command_name)
+    @bot.tree.command(
+        name=delmatch_options.invoke_with,
+        description=delmatch_options.description
+    )
     @discord.app_commands.describe(
         **{
             param: info.helptext
@@ -149,56 +90,16 @@ def setup_bot():
     )
     async def del_match(
             interaction: discord.Interaction,
-            home: discord.Role,
-            away: discord.Role
+            team_1: discord.Role,
+            team_2: discord.Role
     ):
-        LOGGER.info('Canceling the match: %s vs %s', away.name, home.name)
-        with matchlist as db:
-            cancelled_match = db.cancel_match(home.id, away.id).fetchone()
-            print(f'{cancelled_match=}')
-            if cancelled_match is not None:
-                await interaction.response.send_message(
-                    embed=make_cancellation_success_message(
-                        interaction,
-                        home,
-                        away,
-                        delmatch_options.direct_response_ok
-                    ),
-                    ephemeral=True
-                )
-                for channel in delmatch_options.task_completed_message.output_channels:
-                    if channel.log_to_text_channel:
-                        await make_match_callendar_modified_announcement(
-                            delmatch_options.task_completed_message,
-                            interaction.guild.get_channel(
-                                channel.text_channel_id
-                            ),
-                            home,
-                            away,
-                            int(cancelled_match[0])
-                        )
-            else:
-                await interaction.response.send_message(
-                    embed=make_cancellation_failure_message(
-                        interaction,
-                        home,
-                        away,
-                        delmatch_options.direct_response_not_ok
-                    ),
-                    ephemeral=True
-                )
+        pass
 
-    @bot.tree.command(name=showmatches_options.command_name)
+    @bot.tree.command(
+        name=showmatches_options.invoke_with,
+        description=showmatches_options.description
+    )
     async def show_matches(interaction: discord.Interaction):
-        with matchlist as db:
-            matches = db.find_matches()
-            await interaction.response.send_message(
-                embed=make_match_calendar_message(
-                    interaction,
-                    matches,
-                    showmatches_options.direct_response_ok
-                ),
-                ephemeral=True
-            )
+        pass
 
     return bot
--- src/match_scheduler_bot/model/config.py
+++ src/match_scheduler_bot/model/config.py
@@ -4,58 +4,16 @@
     :module_author: CountTails
 """
 
-from typing import List, Dict, Set, Optional, Any
+from typing import List, Dict, Literal
 from typing_extensions import Annotated
 import pathlib
-import re
 
 import pydantic
-
-from ..exceptions import MissingConfigurationError
 
 
 class BotAuthInfo(pydantic.BaseModel):
     token: pydantic.SecretStr
     intents: Dict[str, bool]
-
-
-class OutputChannel(pydantic.BaseModel):
-    log_to_text_channel: pydantic.StrictBool
-    text_channel_id: Annotated[pydantic.StrictInt, pydantic.Field(gt=0)]
-
-
-class EmbedResponseField(pydantic.BaseModel):
-    name: Annotated[pydantic.StrictStr, pydantic.Field(max_length=256)]
-    value: Annotated[pydantic.StrictStr, pydantic.Field(max_length=1024)]
-    inline: pydantic.StrictBool
-
-
-class EmbedResponseFooter(pydantic.BaseModel):
-    text: Annotated[pydantic.StrictStr, pydantic.Field(max_length=2048)]
-
-
-class DiscordBotEmbedResponseTemplate(pydantic.BaseModel):
-    title: Annotated[pydantic.StrictStr, pydantic.Field(max_length=256)]
-    description: Annotated[pydantic.StrictStr, pydantic.Field(max_length=4096)]
-    color: Annotated[
-        pydantic.StrictStr,
-        pydantic.Field(
-            min_length=7,
-            max_length=7,
-            pattern=re.compile(r'^#[0-9A-Fa-f]{6}$')
-        )
-    ]
-    fields: Dict[str, EmbedResponseField]
-    footnote: Optional[EmbedResponseFooter]
-    output_channels: Optional[List[OutputChannel]]
-    output_mentions: Optional[List[int]]
-
-    def get_field(self, field: str):
-        if field in self.fields:
-            return self.fields.get(field)
-        raise MissingConfigurationError(
-            f'Unknown field: {field} in {self.title} embed'
-        )
 
 
 class PersistentStorage(pydantic.BaseModel):
@@ -68,31 +26,22 @@ class CommandParameterInfo(pydantic.BaseModel):
     helptext: Annotated[pydantic.StrictStr, pydantic.Field(min_length=1)]
 
 
-class BotCommand(pydantic.BaseModel):
-    command_name: Annotated[pydantic.StrictStr, pydantic.Field(min_length=1)]
+class CommandSpec(pydantic.BaseModel):
+    invoke_with: Annotated[pydantic.StrictStr, pydantic.Field(min_length=1)]
+    description: Annotated[pydantic.StrictStr, pydantic.Field(min_length=1)]
     parameters: Dict[str, CommandParameterInfo]
-    direct_response_ok: Optional[DiscordBotEmbedResponseTemplate]
-    direct_response_not_ok: Optional[DiscordBotEmbedResponseTemplate]
-    task_completed_message: Optional[DiscordBotEmbedResponseTemplate]
-
-    def __eq__(self, other: Any) -> bool:
-        if isinstance(other, BotCommand):
-            return self.command_name == other.command_name
-        if isinstance(other, str):
-            return self.command_name == other
-        return False
+    allowed_roles: List[int | str]
 
 
 class BotConfig(pydantic.BaseModel):
     auth: BotAuthInfo
-    commands: List[BotCommand]
+    commands: Dict[
+        Literal[
+            'create_match',
+            'delete_match',
+            'list_matches',
+            'command_help'
+        ],
+        CommandSpec
+    ]
     storage: PersistentStorage
-
-    def get_command_info(self, cmd: str) -> BotCommand:
-        for cmdinfo in self.commands:
-            if cmdinfo == cmd:
-                return cmdinfo
-        else:
-            raise MissingConfigurationError(
-                f'No such command configured: {cmd}'
-            )
