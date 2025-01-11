4adb4d9c8e1c223b3d97f93076054494771021ce
CountTails <74679703+CountTails@users.noreply.github.com>
2025-01-10 16:11:34 -0800

    Refactor(bot-config): reintroduce command specification and output directions in config file

--- src/match_scheduler_bot/model/__init__.py
+++ src/match_scheduler_bot/model/__init__.py
@@ -5,7 +5,7 @@
 '''
 
 from pathlib import Path
-from typing import Optional, Dict
+from typing import List, Dict, Annotated, Optional
 
 
 import pydantic
@@ -16,6 +16,25 @@ class BotAuthInfo(pydantic.BaseModel):
     intents: Dict[str, bool]
 
 
+class CommandOutput(pydantic.BaseModel):
+    channel_id: Annotated[int, pydantic.Field(gt=0)]
+    mention: List[int]
+
+
+class CommandOutputDestination(pydantic.BaseModel):
+    public: Optional[CommandOutput]
+    audit: Optional[CommandOutput]
+
+
+class CommandSpec(pydantic.BaseModel):
+    invoke_with: Annotated[pydantic.StrictStr, pydantic.Field(min_length=1)]
+    description: Annotated[pydantic.StrictStr, pydantic.Field(min_length=1)]
+    parameters: Annotated[Dict[str, str], pydantic.Field(default={})]
+    renames: Annotated[Dict[str, str], pydantic.Field(default={})]
+    allowlist: Optional[List[int | str]]
+    respond: CommandOutputDestination
+
+
 class DataSources(pydantic.BaseModel):
     database: str | Path
     # timezones: Set[str]
@@ -23,4 +42,5 @@ class DataSources(pydantic.BaseModel):
 
 class BotConfig(pydantic.BaseModel):
     auth: BotAuthInfo
+    cmds: Dict[str, CommandSpec]
     data: DataSources
