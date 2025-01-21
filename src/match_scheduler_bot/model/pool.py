a2a773e3ab06c4901bb34b798fce3a4915d5f8fc
CountTails <74679703+CountTails@users.noreply.github.com>
2025-01-21 14:52:44 -0800

    Doc(async-dbpool): added docstring comments to dbpool methods

--- src/match_scheduler_bot/model/pool.py
+++ src/match_scheduler_bot/model/pool.py
@@ -5,11 +5,7 @@
 '''
 
 import logging
-import time
 import asyncio
-from concurrent.futures import ThreadPoolExecutor
-from dataclasses import asdict
-from typing import List
 
 import aiosqlite
 
@@ -19,26 +15,55 @@ __LOGGER__ = logging.getLogger(__name__)
 
 class AsyncConnectionPool:
     def __init__(self, path: str, pool_size: int):
+        '''
+            Lazily initializes the connection pool
+            Parameters:
+                path [str]: the path of the dbfile for these connections
+                pool_size [int]: the number of connection to put in the pool
+            Returns:
+                None
+        '''
         self._dbpath = path
         self._sz = pool_size
         self._pool = None
 
     @property
-    def available_connections(self):
+    def available_connections(self) -> int:
+        '''
+            Returns the number of connection currently in the pool
+            or the number of requested connections pool is not initialized yet
+            Returns:
+                [int]: the number of connections
+        '''
         return self._pool.qsize() if self._pool else self._sz
 
     @property
-    def database_path(self):
+    def database_path(self) -> str:
+        '''
+            Returns the path of the dbfile for this connection pool
+            Returns:
+                [str]: path of the connections in this pool
+        '''
         return self._dbpath
 
-    async def _initialize_pool(self):
+    async def _initialize_pool(self) -> None:
+        '''
+            Asyncronous initialization of connections in this pool
+        '''
         self._pool = asyncio.Queue(maxsize=self._sz)
         for _ in range(self._sz):
             self._pool.put_nowait(
                 await aiosqlite.connect(self._dbpath)
             )
 
-    async def acquire(self, row_factory=aiosqlite.Row):
+    async def acquire(self, row_factory=aiosqlite.Row) -> aiosqlite.Connection:
+        '''
+            Get the next available connection from this pool
+            Parameters:
+                row_factory [callable]: factory function to convert sqlite rows
+            Returns:
+                conn [aiosqlite.Connection]: connection from the pool
+        '''
         if not self._pool:
             await self._initialize_pool()
 
@@ -46,10 +71,22 @@ class AsyncConnectionPool:
         conn.row_factory = row_factory
         return conn
 
-    async def release(self, connection: aiosqlite.Connection):
+    async def release(self, connection: aiosqlite.Connection) -> None:
+        '''
+            Return the given connection to this pool
+            Parameters:
+                connection [aiosqlite.Connection]: connection to return
+            Returns:
+                None
+        '''
         await self._pool.put(connection)
 
-    async def destroy(self):
+    async def destroy(self) -> None:
+        '''
+            Close all connections in this pool
+        '''
         while self._pool and not self._pool.empty():
             conn = await self._pool.get()
             await conn.close()
+
+        self._pool = None
