96ba41d14e0d2299906f198bb6e8bca826f68541
CountTails <74679703+CountTails@users.noreply.github.com>
2025-02-06 20:18:29 -0800

    Feat(bot-config): adjust config structure to accomodate the use of command groups. Deduplicate specification of output channels

--- src/match_scheduler_bot/model/__init__.py
+++ src/match_scheduler_bot/model/__init__.py
@@ -5,7 +5,7 @@
 '''
 
 from pathlib import Path
-from typing import List, Dict, Annotated, Optional
+from typing import List, Dict, Annotated, Optional, Union
 from .filecache import FileCacheProvider
 from .pool import AsyncConnectionPool
 from .rows import (
@@ -19,20 +19,16 @@ from .runners import MatchlistQueryRunner
 import pydantic
 
 
+class BotOutput(pydantic.BaseModel):
+    public_log: Annotated[int, pydantic.Field(gt=0)]
+    audit_log: Annotated[int, pydantic.Field(gt=0)]
+
+
 class BotAuthInfo(pydantic.BaseModel):
     token: pydantic.SecretStr
-    intents: Dict[str, bool]
+    intents: Dict[str, pydantic.StrictBool]
     server: Annotated[int, pydantic.Field(gt=0)]
-
-
-class CommandOutput(pydantic.BaseModel):
-    channel_id: Annotated[int, pydantic.Field(gt=0)]
-    mention: List[int]
-
-
-class CommandOutputDestination(pydantic.BaseModel):
-    public: Optional[CommandOutput]
-    audit: Optional[CommandOutput]
+    logs: BotOutput
 
 
 class CommandSpec(pydantic.BaseModel):
@@ -41,7 +37,15 @@ class CommandSpec(pydantic.BaseModel):
     parameters: Annotated[Dict[str, str], pydantic.Field(default={})]
     renames: Annotated[Dict[str, str], pydantic.Field(default={})]
     allowlist: Optional[List[int | str]]
-    respond: CommandOutputDestination
+
+
+class GroupSpec(pydantic.BaseModel):
+    group_module: Annotated[pydantic.StrictStr, pydantic.Field(min_length=1)]
+    group_name: Annotated[pydantic.StrictStr, pydantic.Field(min_length=1)]
+    group_description: Annotated[
+        pydantic.StrictStr, pydantic.Field(min_length=1)
+    ]
+    group_commands: Dict[str, CommandSpec]
 
 
 class DataSources(pydantic.BaseModel):
@@ -52,5 +56,5 @@ class DataSources(pydantic.BaseModel):
 
 class BotConfig(pydantic.BaseModel):
     auth: BotAuthInfo
-    cmds: Dict[str, CommandSpec]
+    commands: Dict[str, Union[GroupSpec, CommandSpec]]
     data: DataSources
