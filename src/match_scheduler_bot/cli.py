69d23d9c12c68d0162dc1f6c570f03cfcd8e34df
CountTails <74679703+CountTails@users.noreply.github.com>
2025-02-06 20:19:20 -0800

    Feat(bot-cli): create a command group and subdivide the bot service callback and bot config check callback

--- src/match_scheduler_bot/cli.py
+++ src/match_scheduler_bot/cli.py
@@ -9,9 +9,16 @@ import os.path
 import asyncio
 
 from match_scheduler_bot.bot import startup
+from match_scheduler_bot.exceptions import BotConfigurationError
+from match_scheduler_bot import setup_config
 
 
-@click.command()
+@click.group
+def main():
+    pass
+
+
+@main.command()
 @click.option(
     '--bot-config',
     type=click.Path(exists=True),
@@ -22,6 +29,23 @@ from match_scheduler_bot.bot import startup
     type=click.Path(exists=True),
     default=f'{os.path.expanduser("~/.config/msa/logging.json")}'
 )
-def main(bot_config, log_config):
+def serve(bot_config, log_config):
     """Entry point to matchschedulerbot"""
     asyncio.run(startup(bot_config, log_config))
+
+
+@main.command()
+@click.option(
+    '--bot-config',
+    type=click.Path(exists=True),
+    default=f'{os.path.expanduser("~/.config/msa/bot.json")}'
+)
+def check(bot_config):
+    """Check if the given config file is valid"""
+    try:
+        setup_config(bot_config)
+        click.echo('Configuration is valid')
+    except BotConfigurationError as err:
+        click.echo(
+            err.__cause__.errors()
+        )
