003bf9ce1c4693496b82ea7fbcc0328feda20f43
CountTails <74679703+CountTails@users.noreply.github.com>
2025-02-15 15:19:58 -0800

    Chore(module-rename): Untrack files from old module name directory

--- src/match_scheduler_bot/model/runners/__init__.py
+++
@@ -1,7 +0,0 @@
-'''
-    :module_name: runners
-    :module_summary: query runners classes handling database interactions
-    :module_author: CountTails
-'''
-
-from .matchlist import MatchlistQueryRunner
