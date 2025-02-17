63650d7773a268c66718f6e6845b6cb712d2102d
CountTails <74679703+CountTails@users.noreply.github.com>
2025-02-16 21:24:24 -0800

    Chore(module-rename): migrate the abstract data types module to the new source directory

---
+++ src/msadiscordapp/model/adts.py
@@ -0,0 +1,127 @@
+'''
+    :module_name: adts
+    :module_summary: hub for abstract data types used in this package
+    :module_author: CountTails
+'''
+
+from __future__ import annotations
+from typing import Tuple
+from dataclasses import dataclass
+from datetime import datetime, timezone
+
+import aiosqlite
+
+
+@dataclass
+class MatchupDetails:
+    '''
+        An data class for representing the two teams facing off
+
+        Attributes:
+            team_1_id [int]: discord role id of the first team in this matchup
+            team_2_id [int]: discord role id of the second team in this Matchup
+
+        Methods:
+            cls fixed_order(): constructs instance so id attributes are ordered
+    '''
+    team_1_id: int
+    team_2_id: int
+
+    @classmethod
+    def fixed_order(
+        cls: MatchupDetails,
+        team1: int,
+        team2: int
+    ) -> MatchupDetails:
+        '''
+            Alternative constructor that ensures the following:
+                - id attributes are ordered (team1 <= team2)
+                - order ids are passed does not affect internal structuring
+
+            Parameters:
+                team1 [int]: discord role id of team 1
+                team2 [int]: discord role id of team 2
+
+            Returns:
+                MatchupDetails -> new instance with ensured ordering of ids
+        '''
+        if team1 > team2:
+            team1, team2 = team2, team1
+        return cls(team1, team2)
+
+
+@dataclass
+class MatchToSchedule(MatchupDetails):
+    '''
+        A data class that represents a request to schedule a match
+
+        Attributes:
+            proposed_start [datetime]: proposed starting time of the match
+
+        Methods:
+            cls fixed_order(): constructs instance so id attributes are ordered
+    '''
+    proposed_start: datetime
+
+    @classmethod
+    def fixed_order(
+        cls: MatchToSchedule,
+        team1: int,
+        team2: int,
+        start_at: datetime
+    ) -> MatchToSchedule:
+        matchup = MatchupDetails.fixed_order(team1, team2)
+        return cls(
+            matchup.team_1_id,
+            matchup.team_2_id,
+            start_at,
+        )
+
+
+@dataclass
+class MatchToCancel(MatchupDetails):
+    '''
+        A data class that represents a request to cancel a match
+    '''
+
+
+@dataclass
+class ScheduledMatch:
+    '''
+        A data class that represents a scheduled match in the matchlist
+
+        Attributes:
+            start_at [datetime]: starting date/time of the match
+            team1 [int]: first participating team in the matchup
+            team2 [int]: second participating team in the matchup
+
+        Methods:
+            cls from_row(): destructures an SQL table row into an instance
+    '''
+    start_at: datetime
+    team1: int
+    team2: int
+
+    @classmethod
+    def from_row(
+        cls: ScheduledMatch,
+        cursor: aiosqlite.Cursor,
+        row: Tuple[int]
+    ) -> ScheduledMatch:
+        '''
+            Destructures a row from an SQL table in the form (int, int, int)
+            into an instance of this class.
+
+            Parameters:
+                cursor [aiosqlite.Cursor]: pointer to the source of the SQL row
+                row [Tuple[int]]: raw data from the SQL row
+
+            Returns:
+                ScheduledMatch -> interpreted details of the match row
+        '''
+        dt, t1, t2 = row
+        return cls(
+            start_at=datetime.fromtimestamp(dt, timezone.utc),
+            team1=t1,
+            team2=t2
+        )
